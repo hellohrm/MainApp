a23d716bdc5ab9f8b9522c8eeead5e2a6437a6d1
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-08 19:29:36 +0700

    v1

--- apiPHP/app/Http/Controllers/ApiController.php
+++ apiPHP/app/Http/Controllers/ApiController.php
@@ -30,9 +30,56 @@ class ApiController extends Controller
 
     public function updateStudent(Request $request, $id) {
         // logic to update a student record goes here
+        //if (Student::where('id', $id)->exists()) {
+        //    $student = Student::find($id);
+        //    $student->name = is_null($request->name) ? $student->name : $request->name;
+        //    $student->course = is_null($request->course) ? $student->course : $request->course;
+        //    $student->save();
+
+        //    return response()->json([
+        //        "message" => "records updated successfully"
+        //    ], 200);
+        //} else {
+        //    return response()->json([
+        //        "message" => "Student not found"
+        //    ], 404);
+
+        //}
+
+            $student = new Student;
+            $student->id = $request->id;
+            $student->name = $request->name;
+            $student->course = $request->course;
+
+            return response()->json([
+                "message" => "updateStudent"
+            ], 201);
     }
 
-    public function deleteStudent ($id) {
+    public function deleteStudent (Request $request,$id) {
         // logic to delete a student record goes here
+        //if(Student::where('id', $id)->exists()) {
+        //    $student = Student::find($id);
+        //    $student->delete();
+
+        //    return response()->json([
+        //      "message" => "records deleted"
+        //    ], 202);
+        //} else {
+        //    return response()->json([
+        //      "message" => "Student not found"
+        //    ], 404);
+        //}
+
+
+        $student = new Student;
+        $student->id = $request->id;
+        $student->name = $request->name;
+        $student->course = $request->course;
+
+        return response()->json([
+            "message" => "deleteStudent"
+        ], 201);
+
     }
 }
